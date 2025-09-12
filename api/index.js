fda43eb1bf8264afb70ccd50c1a0b1a4d6b38af6
Julio Cesar Garcia <jcgarcia@ingasti.com>
2025-09-12 15:09:58 +0100

    Fix: Add X-API-Key to CORS allowed headers for publish endpoint

--- api/index.js
+++ api/index.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-API-Key");
   
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
